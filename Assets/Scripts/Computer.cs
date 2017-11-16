232996edfce4bc6131b74ea243525980dd41aad4
galhajaj <galhajaj@gmail.com>
2017-11-16 14:30:59 +0200

    computer on/off click

--- Assets/Scripts/Computer.cs
+++ Assets/Scripts/Computer.cs
@@ -2,21 +2,40 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Computer : MonoBehaviour {
+public class Computer : MonoBehaviour
+{
+    private bool _isOn;
+    public bool IsOn
+    {
+        get { return _isOn; }
+        set
+        {
+            _isOn = value;
+            updateScreenColor();
+        }
+    }
 
-    public string name;
-	// Use this for initialization
-	void Start () {
-		
+    void Start ()
+    {
+        IsOn = true;
 	}
 	
-	// Update is called once per frame
-	void Update () {
+	void Update ()
+    {
 		
 	}
 
     void OnMouseDown()
     {
         Debug.Log("clicked Computer ");
+        IsOn = !IsOn;
+    }
+
+    private void updateScreenColor()
+    {
+        if (_isOn)
+            transform.GetChild(0).GetComponent<SpriteRenderer>().color = Color.blue;
+        else
+            transform.GetChild(0).GetComponent<SpriteRenderer>().color = Color.red;
     }
 }
