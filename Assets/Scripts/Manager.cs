e467828a48077b56cf52224492c167697ea30dc4
galhajaj <galhajaj@gmail.com>
2017-11-16 09:23:05 +0200

    init

---
+++ Assets/Scripts/Manager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Manager : MonoBehaviour
+{
+
+	void Start ()
+    {
+        Debug.Log("Startttttt");
+	}
+	
+	void Update ()
+    {
+		
+	}
+}
