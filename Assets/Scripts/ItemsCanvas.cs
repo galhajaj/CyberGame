0e3ef052adcb6c1e08e411de142594094974bb90
galhajaj <galhajaj@gmail.com>
2017-11-16 19:12:35 +0200

    add rooms - nice!

---
+++ Assets/Scripts/ItemsCanvas.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ItemsCanvas : MonoBehaviour
+{
+    public static ItemsCanvas Instance = null;
+
+    void Awake()
+    {
+        if (Instance == null)
+            Instance = this;
+
+        else if (Instance != this)
+            Destroy(gameObject);
+
+        DontDestroyOnLoad(gameObject);
+    }
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+
+    }
+}
