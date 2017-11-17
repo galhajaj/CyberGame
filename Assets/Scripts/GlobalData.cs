e98d23efb69a42262d01d211c33b3e29421f80d6
galhajaj <galhajaj@gmail.com>
2017-11-17 16:08:20 +0200

    add GlobalData

---
+++ Assets/Scripts/GlobalData.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlobalData : MonoBehaviour
+{
+    public static GlobalData Instance = null;
+
+    void Awake()
+    {
+        if (Instance == null)
+            Instance = this;
+
+        else if (Instance != this)
+            Destroy(gameObject);
+
+        DontDestroyOnLoad(gameObject);
+    }
+
+    // doors
+    public bool IsDoorOpen_Start2Outside = false;
+    public bool IsDoorOpen_Start2Ladder = false;
+    public bool IsDoorOpen_Start2Stairs = false;
+
+    // items
+    public bool IsRopeTaken = false;
+    public bool IsRopeOnLadder = false;
+
+
+    void Start ()
+    {
+		
+	}
+	
+	void Update ()
+    {
+		
+	}
+}
