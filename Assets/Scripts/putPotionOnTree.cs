a8d3fcf58b80b665b27f7c136b64c7ff965e583f
galhajaj <galhajaj@gmail.com>
2017-11-19 12:41:52 +0200

    add tree fruits etc

---
+++ Assets/Scripts/putPotionOnTree.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class putPotionOnTree : MonoBehaviour
+{
+    public GameObject FruitsOnTree;
+    public GameObject RedFruitOnGround;
+    public GameObject RedFruitOnGround2;
+    public GameObject BlueFruitOnGround;
+    public GameObject BlueFruitOnGround2;
+    public GameObject YellowFruitOnGround;
+    public GameObject YellowFruitOnGround2;
+
+    private bool _isFruitsOnTree = false;
+    private bool _isFruitsOnGround = false;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnMouseDown()
+    {
+        if (ItemsManager.Instance.CurrentItem == "Potion")
+        {
+            ItemsManager.Instance.RemoveItem("Potion");
+            _isFruitsOnTree = true;
+            FruitsOnTree.SetActive(true);
+        }
+
+        if (ItemsManager.Instance.CurrentItem == "Hammer" && !_isFruitsOnGround && _isFruitsOnTree)
+        {
+            _isFruitsOnGround = true;
+            ItemsManager.Instance.UnselectAllItems();
+            RedFruitOnGround.SetActive(true);
+            RedFruitOnGround2.SetActive(true);
+            BlueFruitOnGround.SetActive(true);
+            BlueFruitOnGround2.SetActive(true);
+            YellowFruitOnGround.SetActive(true);
+            YellowFruitOnGround2.SetActive(true);
+        }
+    }
+}
