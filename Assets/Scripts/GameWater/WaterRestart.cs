37b8c9b5c82daa886d1ef0c52d81c4443ff226db
galhajaj <galhajaj@gmail.com>
2017-11-19 18:31:21 +0200

    please make it stop
    This reverts commit 76010bbc36f1f1d60466e4e5b5e4d0b90c1a67a6.

--- Assets/Scripts/GameWater/WaterRestart.cs
+++ Assets/Scripts/GameWater/WaterRestart.cs
@@ -7,7 +7,11 @@ public class WaterRestart : MonoBehaviour {
     WaterVail _vail;
 	// Use this for initialization
 	void Start () {
+<<<<<<< HEAD
         _vail = GameObject.Find("Vail").GetComponent<WaterVail>();
+=======
+        _vail = transform.parent.Find("Vail").GetComponent<WaterVail>();
+>>>>>>> parent of 76010bb... added games to gamescene
 	}
 	
 	// Update is called once per frame
